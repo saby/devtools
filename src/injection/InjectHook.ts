5ebbdafbfa52e1ae45924bb65541dd43781d672d
as.zaycev <as.zaycev@tensor.ru>
2019-04-18 16:58:53 +0300

    communication system

--- src/injection/InjectHook.ts
+++ src/injection/InjectHook.ts
@@ -1,7 +1,6 @@
 import { IPlugin } from './IPlugin';
 import { Hook } from './_hook/Hook';
 
-
 export class InjectHook implements IPlugin {
    constructor() {
       if (window.__WASABY_DEV_HOOK__) {
