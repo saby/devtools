4e2ed6795439f84001ca13b88cb066f1e6e83c13
av.zalyaev <av.zalyaev@tensor.ru>
2019-04-22 15:47:10 +0300

    remove channel

--- src/interface/IChannel.d.ts
+++
@@ -1,6 +0,0 @@
-import { IDevToolsEvent } from './IDevToolsEvent';
-
-export interface IChannel {
-   send(message: IDevToolsEvent): void;
-   listen(callback: (message: IDevToolsEvent) => void): void;
-}
