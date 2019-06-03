de3dc0d4272cc88b3d0a8e19f512fd55059160da
av.zalyaev <av.zalyaev@tensor.ru>
2019-06-03 15:41:35 +0300

    fix inject

--- src/extension/content/injectScript.ts
+++ src/extension/content/injectScript.ts
@@ -2,9 +2,10 @@ let injectScript = (fileName: string) => {
     let scriptElement = document.createElement("script");
     scriptElement.setAttribute("type", "text/javascript");
     scriptElement.src = chrome.extension.getURL(fileName);
-    setTimeout(() => {
-        document.head.appendChild(scriptElement);
-    }, 100)
+    document.documentElement.appendChild(scriptElement);
+    // setTimeout(() => {
+    //     document.head.appendChild(scriptElement);
+    // }, 100)
 };
 
 export { injectScript };
