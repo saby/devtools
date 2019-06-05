6603b61f722fdf9cfd3598e5418d7f5eadb64a65
av.zalyaev <av.zalyaev@tensor.ru>
2019-06-05 11:52:27 +0300

    class Define: IDescriptor

---
+++ src/injection/_dependencyWatcher/Define.ts
@@ -0,0 +1,30 @@
+import { wrapDefineDynamic } from './define/dynamic';
+import { IConfigWithStorage } from "./IConfig";
+import { IDefine } from "./define/IDefine";
+import { IDescriptor } from "./IDescriptor";
+import { ModuleStorage } from "./ModuleStorage";
+import { ILogger } from "Extension/Logger/ILogger";
+
+export class Define implements IDescriptor {
+    constructor({ logger, moduleStorage }: IConfigWithStorage) {
+        this.__storage = moduleStorage;
+        this.__logger = logger;
+    }
+    private __storage: ModuleStorage;
+    private __logger: ILogger;
+    private __define: IDefine;
+    private __proxy: IDefine;
+    getDescriptor(): PropertyDescriptor {
+        let _this = this;
+        return {
+            set(value: IDefine) {
+                _this.__define = value;
+                _this.__proxy = wrapDefineDynamic(_this.__define, _this.__storage, _this.__logger);
+            },
+            get(): IDefine | void {
+                return _this.__proxy;
+            }
+        }
+    
+    }
+}
