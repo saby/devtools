882e4cd2b94b637713162250fedd36fc23ff0729
av.zalyaev <av.zalyaev@tensor.ru>
2019-06-05 11:53:44 +0300

    class Require: IDescriptor

--- src/injection/_dependencyWatcher/require/proxy.ts
+++ src/injection/_dependencyWatcher/require/proxy.ts
@@ -1,31 +1,35 @@
 import { IRequire } from "./IRequire";
-import { moduleStorage } from "../moduleStorage";
+import { ModuleStorage, moduleStorage } from "../moduleStorage";
 import { GLOBAL_MODULE_NAME } from "Extension/Plugins/DependencyWatcher/const";
+import { ILogger } from "Extension/Logger/ILogger";
 
-let addDeps = (argArray: any[]) => {
-    try {
-        let deps = argArray[0];
-        if (typeof deps == 'string') {
-            moduleStorage.addDependency(GLOBAL_MODULE_NAME, [deps]);
-        } else if (Array.isArray(deps)) {
-            moduleStorage.addDependency(GLOBAL_MODULE_NAME, deps);
-        } else if (typeof deps == 'object' && Array.isArray(argArray[1])) {
-            moduleStorage.addDependency(GLOBAL_MODULE_NAME, argArray[1]);
+let addDepsFunction = (storage: ModuleStorage, logger: ILogger) => {
+    return (argArray: any[]) => {
+        try {
+            let deps = argArray[0];
+            if (typeof deps == 'string') {
+                storage.require(GLOBAL_MODULE_NAME, [deps]);
+            } else if (Array.isArray(deps)) {
+                storage.require(GLOBAL_MODULE_NAME, deps);
+            } else if (typeof deps == 'object' && Array.isArray(argArray[1])) {
+                storage.require(GLOBAL_MODULE_NAME, argArray[1]);
+            }
         }
-    }
-    catch (e) {
-    
-    }
+        catch (error) {
+            logger.error(error);
+        }
+    };
 };
 
-export let proxyRequire = (require: IRequire) => {
+export let proxyRequire = (require: IRequire, storage: ModuleStorage, logger: ILogger) => {
+    let addDeps = addDepsFunction(storage, logger);
     return new Proxy(require, {
         apply(
             target: any,
             thisArg: any,
             argArray: any[]
         ): any {
-            addDeps(thisArg);
+            addDeps(argArray);
             return target.apply(thisArg, argArray);
         }
     });
