9daf1414e338877fc80987cc6de434ee68556601
av.zalyaev <av.zalyaev@tensor.ru>
2019-06-14 16:52:05 +0300

    bugfix find in module (withouut for of Map)

--- src/app/DependencyWatcher/_data/source/util/findModule.ts
+++ src/app/DependencyWatcher/_data/source/util/findModule.ts
@@ -1,9 +1,20 @@
 import { Module, ModulesMap } from "Extension/Plugins/DependencyWatcher/IModule";
 
 export let findModule = (allModules: ModulesMap, id: string | number): Module | void => {
-    for (let [ name, module ] of allModules) {
-        if (module.id == id) {
-            return module;
+    // todo до дех пор, пока TS не научится нормально компилировать for of Map в es5, либо билдер не науится чобирать в es6+
+    // for (let [ name, module ] of allModules) {
+    //     if (module.id == id) {
+    //         return module;
+    //     }
+    // }
+    let module: Module;
+    allModules.forEach((m) => {
+        if (module) {
+            return;
         }
-    }
+        if (m.id == id) {
+            module = m;
+        }
+    });
+    return module;
 };
