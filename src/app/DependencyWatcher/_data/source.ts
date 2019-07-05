9ce4b40a4b51848301f3778effec45e7973bdb23
av.zalyaev <av.zalyaev@tensor.ru>
2019-07-05 13:57:24 +0300

    file source

--- src/app/DependencyWatcher/_data/source.ts
+++ src/app/DependencyWatcher/_data/source.ts
@@ -1,3 +1,4 @@
 export { Dependencies } from './source/Dependencies'
 export { Dependent } from './source/Dependent'
+export { File } from './source/File'
 export { ISourceConfig as IConfig, Abstract } from './source/Abstract'
---
+++ src/app/DependencyWatcher/_data/source/File.ts
@@ -0,0 +1,35 @@
+import { Memory, DataSet, Query } from 'Types/source';
+
+import { RPC } from "Extension/Event/RPC";
+import { RPCMethods } from "../RPCMethods";
+import { IFile } from "Extension/Plugins/DependencyWatcher/IFile";
+import { register } from "Types/di";
+
+export interface ISourceConfig {
+    rpc: RPC
+}
+
+export class File extends Memory {
+    private _rpc: RPCMethods;
+    constructor(config: ISourceConfig) {
+        // @ts-ignore
+        super(config);
+        this._rpc = new RPCMethods(config.rpc);
+    }
+    // @ts-ignore
+    query(query: Query): Promise<DataSet> {
+        return this._rpc.getFiles().then((files: Map<number, IFile>) => {
+            this._$data = Array.from(files, ([id, file]) => {
+                return file;
+            });
+            return super.query(query);
+        });
+    }
+}
+
+Object.assign(File.prototype, {
+    '[DependencyWatcher/_data/source/File]': true,
+    _moduleName: 'DependencyWatcher/data:source.File'
+});
+
+register('DependencyWatcher/data:source.File', File, {instantiate: false});
