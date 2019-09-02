68d91b172acbe839910f4180bfca128aa9f41059
as.zaycev <as.zaycev@tensor.ru>
2019-09-02 15:40:35 +0300

    fixed tslint errors fixed formatting

--- src/shared/Extension/Plugins/DependencyWatcher/data/filter/itemFilters.ts
+++ src/shared/Extension/Plugins/DependencyWatcher/data/filter/itemFilters.ts
@@ -1,5 +1,8 @@
-import { FilterFunctionGetter } from "Extension/Plugins/DependencyWatcher/data/filter/Filter";
-import { IRPCModuleFilter, IRPCModuleInfo } from 'Extension/Plugins/DependencyWatcher/IRPCModule';
+import { FilterFunctionGetter } from 'Extension/Plugins/DependencyWatcher/data/filter/Filter';
+import {
+   IRPCModuleFilter,
+   IRPCModuleInfo
+} from 'Extension/Plugins/DependencyWatcher/IRPCModule';
 import { getForName } from 'Extension/Plugins/DependencyWatcher/data/filter/getForName';
 import { getForFiles } from 'Extension/Plugins/DependencyWatcher/data/filter/getForFiles';
 import ignoreWrap from 'Extension/Plugins/DependencyWatcher/data/filter/ignoreWrap';
@@ -7,14 +10,14 @@ import { css } from 'Extension/Plugins/DependencyWatcher/data/filter/css';
 import { json } from 'Extension/Plugins/DependencyWatcher/data/filter/json';
 import { i18n } from 'Extension/Plugins/DependencyWatcher/data/filter/i18n';
 
-
-const itemFilters: Partial<Record<keyof IRPCModuleFilter, FilterFunctionGetter<any, IRPCModuleInfo>>> = {
-    name: getForName,
-    files: getForFiles,
-    css:  ignoreWrap(css),
-    json: ignoreWrap(json),
-    i18n: ignoreWrap(i18n),
-    // dependentOnFile
+const itemFilters: Partial<
+   Record<keyof IRPCModuleFilter, FilterFunctionGetter<any, IRPCModuleInfo>>
+> = {
+   name: getForName,
+   files: getForFiles,
+   css: ignoreWrap(css),
+   json: ignoreWrap(json),
+   i18n: ignoreWrap(i18n)
 };
 
 export default itemFilters;
