f9649622c8b2539018ed79b9d3e47b2f466f713c
as.zaycev <as.zaycev@tensor.ru>
2019-09-05 09:32:16 +0300

    fixed template name of suggest in filter panel

--- src/app/DependencyWatcher/module.ts
+++ src/app/DependencyWatcher/module.ts
@@ -1,3 +1,5 @@
 import * as filter from './_module/filter';
 export { default as List } from './_module/List';
-export { filter }
+export { filter };
+const suggest = filter.file.suggest;
+export { suggest };
