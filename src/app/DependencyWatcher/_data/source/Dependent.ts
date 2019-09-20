6faa9e470e12ef20c48919460fca42b9e6fe15d8
as.zaycev <as.zaycev@tensor.ru>
2019-09-20 13:08:15 +0300

    removed unused code

--- src/app/DependencyWatcher/_data/source/Dependent.ts
+++ src/app/DependencyWatcher/_data/source/Dependent.ts
@@ -3,28 +3,7 @@ import { ListAbstract } from './ListAbstract';
 import { ITransferRPCModule } from 'Extension/Plugins/DependencyWatcher/IRPCModule';
 
 export class Dependent extends ListAbstract {
-
-   private _pageName: string;
    protected _getChildren(item: ITransferRPCModule): IDependencies<number[]> {
       return item.dependent;
-   }
-   private __getPageName(): Promise<string> {
-      if (this._pageName) {
-         return Promise.resolve(this._pageName);
-      }
-      const DEFAULT = 'page';
-      return new Promise<string>((resolve) => {
-         const timer = setTimeout(resolve, 1000, DEFAULT);
-         chrome.tabs.get(
-            chrome.devtools.inspectedWindow.tabId,
-            (tab: chrome.tabs.Tab) => {
-               resolve(tab.url || DEFAULT);
-               clearTimeout(timer);
-            }
-         );
-      }).then((name: string) => {
-         this._pageName = name;
-         return name;
-      });
    }
 }
