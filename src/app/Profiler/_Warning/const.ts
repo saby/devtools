e3d81ce2ac62aa81b6478395149da755482b07d1
as.zaycev <as.zaycev@tensor.ru>
2019-12-17 11:21:36 +0300

    changed wording in the caption for domUnchanged

--- src/app/Profiler/_Warning/const.ts
+++ src/app/Profiler/_Warning/const.ts
@@ -7,7 +7,7 @@ export type WARNING_NAMES = 'domUnchanged';
 
 export const WARNINGS: Record<WARNING_NAMES, IWarning> = {
    domUnchanged: {
-      caption: 'Unnecessary synchronization',
+      caption: 'Needless synchronization',
       template: 'Profiler/profiler:domUnchanged'
    }
 };
